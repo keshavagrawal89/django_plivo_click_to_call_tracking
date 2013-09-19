39d9832ecbbe4a43605410da428d129eedc854c0
Keshav Agrawal <keshav_agrawal89@hotmail.com>
2013-09-19 14:13:03 +0530

    some typos..

--- clicktocall/models.py
+++ clicktocall/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class Lead(models.Model):
 	duration = models.DecimalField(max_digits=5, decimal_places=2)
-	from_number = models.IntegerField(max_length=999999999999999)
-	to_number = models.IntegerField(max_length=999999999999999)
+	from_number = models.BigIntegerField(max_length=999999999999999)
+	to_number = models.BigIntegerField(max_length=999999999999999)
 

ed593d7da12a00b7507bf98be2c18b2594a896bb
Keshav Agrawal <keshav_agrawal89@hotmail.com>
2013-09-19 23:36:46 +0530

    bit of tracking concept

--- clicktocall/views.py
+++ clicktocall/views.py
@@ -9,11 +9,51 @@ from decimal import Decimal
 import plivo
 from django.views.decorators.csrf import csrf_exempt
 from clicktocall.views import Lead
-
+from django.db import connection
+import datetime
 
 def main_page(request):
 	variables = {'':''}
 	return render_to_response('main_page.html', RequestContext(request, variables))
+
+def track_lead(request):
+	cursor = connection.cursor()
+	cursor.execute("select count(*), to_number, date_of_call from clicktocall_lead group by date_of_call,to_number order by date_of_call, to_number")
+
+	res = cursor.fetchall()
+
+	list_of_numbers = []
+	list_of_dates = []
+	list_of_tuples = []
+
+	final_list = []
+
+	for row in res:
+		list_of_numbers.append(row[1])
+		list_of_dates.append(row[2])
+
+	list_of_tuples = res
+
+	list_of_numbers = list(set(list_of_numbers))
+	list_of_dates = list(set(list_of_dates))
+
+	for number in list_of_numbers:
+		final_list.append([])
+
+	for call_date in list_of_dates:
+		for item in final_list:
+			item.append(0)
+		index_of_date = list_of_dates.index(call_date)
+		for tuple_ in list_of_tuples:
+			if call_date in tuple_:
+				index_of_number = list_of_numbers.index(tuple_[1])
+				final_list[index_of_number][index_of_date] = tuple_[0]
+
+	print final_list
+
+	date_list = list_of_dates
+	variables = {'date_list':date_list, 'numbers':list_of_numbers, 'final_list':final_list}
+	return render_to_response('chart.html', RequestContext(request, variables))
 
 
 def send_call(request):
@@ -61,10 +101,11 @@ def dialagent(request):
 
 @csrf_exempt
 def hangup_lead(request):
+	call_date = datetime.datetime.date(datetime.datetime.now())
 	call_duration = Decimal(request.POST.get('Duration'))
 	caller = int(request.POST.get('From'))
 	agent_number = int(request.POST.get('To'))
-	
-	lead = Lead(from_number = caller, to_number = agent_number, duration = call_duration)
+	lead = Lead(from_number = caller, to_number = agent_number, duration = call_duration, date_of_call = call_date)
+	lead.save()
 	return HttpResponseRedirect('/')
 

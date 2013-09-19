568a47ee27906cb5df1e8ee0e81534d18eef753e
Keshav Agrawal <keshav_agrawal89@hotmail.com>
2013-09-19 23:31:02 +0530

    bit of tracking concept

--- clicktocall/models.py
+++ clicktocall/models.py
@@ -7,4 +7,5 @@ class Lead(models.Model):
 	duration = models.DecimalField(max_digits=5, decimal_places=2)
 	from_number = models.BigIntegerField(max_length=999999999999999)
 	to_number = models.BigIntegerField(max_length=999999999999999)
-
+#Thinking of some data and tracking?
+	date_of_call = models.DateField()
